501dbefe6a6ed684498ffbf3a763d4500f591049
Amongassik <118072713+Amongassik@users.noreply.github.com>
2026-04-05 16:44:16 +0300

    Case

---
+++ app/case/__init__.py
@@ -0,0 +1,9 @@
+"""Модуль CASE-системы для лабораторной работы №11"""
+
+from .state import CaseHandel,CaseState,get_user_case
+
+__all__ = [
+    'CaseState',
+    'CaseHandel',
+    'get_user_case'
+]
--- app/case/state.py
+++ app/case/state.py
@@ -1,11 +1,8 @@
 from aiogram.fsm.state import State,StatesGroup
-from typing import Optional,Dict,Any
+from typing import Optional,Dict
 import math
 
 class CaseState(StatesGroup):
-    for_f1 = State()
-    for_f2 = State()
-    for_f3 = State()
     for_x = State()
 
 class CaseHandel:
@@ -25,26 +22,52 @@ class CaseHandel:
         self.x: Optional[float] = None
         self.result: Optional[float] = None
         self.message:Optional[str] = None
-        self.error: Optional[str] = True
-        self.success:Optional[bool] = None
+        self.error: Optional[str] = None
+        self.success:Optional[bool] = True
     
     def is_ready(self)->bool:
         return (self.f1 is not None) and (self.f2 is not None) and (self.f3 is not None)
     
+    def check_success(self):
+        return self.success
+    
     def set_func(self,func_str:str,func_num:int):
-        if func_num == 1:
+        if func_num == 0:
             self.f1 = func_str
-        elif func_num == 2:
+        elif func_num == 1:
             self.f2 = func_str
-        elif func_num == 3:
+        elif func_num == 2:
             self.f3 = func_str
         else:
             self.success = False
             self.error = "Error Index Value"
+    
+    def set_x(self,x:float):
+        if x is None:
+            self.success = False
+            self.error = "Ошибка:введите число"
+            return
+        if not isinstance(x,(int,float)):
+            self.success = False
+            self.error = f"Ошибка:занчение должно быть числом"
+            return
+        self.x = x
+        self.success = True
+        self.error = None
+        self.result = None
 
     @classmethod
     def to_type_func(cls,func_str:str):
         return cls.FUNC_TYPES.get(func_str,'?')
+    
+    def to_formula(self):
+        if not self.is_ready():
+            self.success = False
+            self.error = None
+            return "Не все функции выбраны.Должно получится выражение: y = [F₁] ( [F₂] ( [F₃] ( x ) ) )"
+        
+        func_str =  f"({self.f1}({self.f2}({self.f3}(x))))"
+        return f" Функция:y={func_str}"
     
     def apply_function(self,func_type:str,x:float):
         if func_type == 'sqrt':
@@ -58,39 +81,45 @@ class CaseHandel:
                 self.success = False
                 self.error = f"Ошибка области определения в {x} при вычислении 1/x"
                 return
-            else:
-                return 1/x
+            return 1/x
         if func_type == 'exp':
             try:
                 return math.exp(x)
             except:
                 self.success = False
                 self.error = f"Ошибка: переполнение при вычислении e^{x}"
+                return
         else:
             self.success = False
             self.error = f"Ошибка типа функции , вы не правильно ввели функцию"
             return
     
-    def calculate(self,x:float):
+    def calculate(self):
+        if not self.is_ready():
+            self.success = False
+            self.error = 'Не все функции заполенны'
+            return
         func_types = [self.to_type_func(f) for f in [self.f3,self.f2,self.f1]]
-        result = x
+        self.success = True
+        if self.x is None:
+            self.success = False
+            self.error = "Не введено значение X"
+            return
+        result = self.x
         for f in func_types:
             result = self.apply_function(f,result)
             if not self.success:
                 return
-        self.message = f"Результат посчитан успешно"
+        self.message = f"Результат посчитан успешно:{result}"
+        self.result = result
     
-    @classmethod
-    def to_vba(cls):
+    def to_vba(self):
         """Возвращает код для VBA в виде строки (пока не надо реализовывать)"""
         pass
 
+user_cases:Dict[int,CaseHandel] = {}
 
-        
-        
-    
-
-            
-    
-
-    
+def get_user_case(user_id:int)->CaseHandel:
+    if user_id not in user_cases:
+        user_cases[user_id] = CaseHandel()
+    return user_cases[user_id]    
